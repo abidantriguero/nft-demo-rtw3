3d08a0024ec08d711e9d9bf404ea2cf1b93bf0bc
Abidan Triguero <abidantriguero@gmail.com>
2022-09-02 13:54:41 -0400

    First Commit

---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <h1>Hello Road to Web3</h1>
+    </div>
+  )
+}
+
+export default Home
