9cf9f7ea07d9c3b248324cc11fbafdc20cad1608
Abidan Triguero <abidantriguero@gmail.com>
2022-09-05 16:24:50 -0400

    First Commit

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,49 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { useState } from 'react'
 
 const Home = () => {
+  const [wallet, setWalletAddress] = useState("");
+  const [collection, setCollectionAddress] = useState("");
+  const [NFTs, setNFTs] = useState([])
+
+  const fetchNFTs = async() => {
+    let nfts; 
+    console.log("fetching nfts");
+    const api_key = "sjt7W1vANjXjPBT-mzlf2euRI1_dFuNp"
+    const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${api_key}/getNFTs/`;
+    
+    if (!collection.length) {
+      var requestOptions = {
+        method: 'GET'
+      };
+
+      const fetchURL = `${baseURL}?owner=${wallet}`;
+  
+      nfts = await fetch(fetchURL, requestOptions).then(data => data.json())
+    } else {
+      console.log("fetching nfts for collection owned by address")
+      const fetchURL = `${baseURL}?owner=${wallet}&contractAddresses%5B%5D=${collection}`;
+      nfts= await fetch(fetchURL, requestOptions).then(data => data.json())
+    }
+  
+    if (nfts) {
+      console.log("nfts:", nfts)
+      setNFTs(nfts.ownedNfts)
+    }
+  }
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <h1>Hello Road to Web3</h1>
+    <div className="flex flex-col items-center justify-center py-8 gap-y-3">
+      <div className="flex flex-col w-full justify-center items-center gap-y-2">
+        <input type={"text"} placeholder="Add your wallet address"></input>
+        <input type={"text"} placeholder="Add the collection address"></input>
+        <label className="text-gray-600 "><input type={"checkbox"} className="mr-2"></input>Fetch for collection</label>
+        <button className={"disabled:bg-slate-500 text-white bg-blue-400 px-4 py-2 mt-3 rounded-sm w-1/5"} onClick={
+          () => {
+          }
+        }>Let's go! </button>
+      </div>
     </div>
   )
 }
